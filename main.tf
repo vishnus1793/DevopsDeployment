fb66952ec895bec509fc847a2f153a10f3c9e676
vishnus1793 <vishnusakthi.44@gmail.com>
2025-04-19 08:34:12 -0400

    testsample main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "portfolio_instance" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  key_name = "" # 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              docker login -u vishnus1793 -p 3q!jJC.dasQxdDC
+              docker pull vishnus1793/portfolio:latest
+              docker run -d -p 80:80 vishnus1793/portfolio
+              EOF
+
+  tags = {
+    Name = "portfolio-instance"
+  }
+}
